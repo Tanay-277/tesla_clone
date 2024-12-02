4deda99395f3bf4788e4a3ec5c0558fb442b1fd6
Tanay-277 <tanayshinde081@gmail.com>
2024-12-02 22:33:18 +0530

    refactor: clean up console logs and update carousel navigation logic

--- src/pages/Vehicles/Configurator.jsx
+++ src/pages/Vehicles/Configurator.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router";
 import { validModels, getModelDetails } from "../../constants/helpers";
 import { pricingData } from "../../constants";
 import { TopBar, Navbar, Pricing, Customization, ConfigSummary } from "../../components";
-import { setInterior, setModel } from "../../features/configurator/pricingSlice";
+import { setExterior, setInterior, setModel } from "../../features/configurator/pricingSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { ChevronLeftIcon } from "@heroicons/react/24/outline";
 import { setImg } from "../../features/configurator/imgSlice";
@@ -12,14 +12,12 @@ const Configurator = () => {
   const { modelName } = useParams();
   const dispatch = useDispatch();
 
-  // Get model details
-  const { name, year, price, img } = getModelDetails(modelName);
+  const { name, img } = getModelDetails(modelName);
 
-  // Get states from Redux
   const imgUrl = useSelector((state) => state.img.img);
   const interior = useSelector((state) => state.pricing.interior);
+  const exterior = useSelector((state) => state.pricing.exterior);
 
-  // Validate model and set title
   useEffect(() => {
     if (!validModels.some(({ name }) => name === modelName)) {
       return;
@@ -28,17 +26,16 @@ const Configurator = () => {
     document.title = `Design Your ${name} | Tesla`;
   }, [modelName, name, dispatch]);
 
-  // Validate model name
+
   if (!validModels.some(({ name }) => name === modelName)) {
     return <h1>Invalid Model Name</h1>;
   }
 
-  // Handle carousel actions
   const handleCarousel = (direction) => {
     if (direction === "left") {
-      dispatch(setImg(img)); // Replace with actual logic for left navigation
+      dispatch(setExterior(exterior));
     } else if (direction === "right") {
-      dispatch(setInterior(interior)); // Replace with actual logic for right navigation
+      dispatch(setInterior(interior));
     } else {
       console.error("Invalid direction");
     }
@@ -48,7 +45,8 @@ const Configurator = () => {
     <div className="size-full min-h-dvh flex flex-col relative overflow-x-hidden">
       <TopBar>
         <div className="text-center">
-          Take delivery by December 31 to receive 3 months of FSD (Supervised) and Supercharging.
+          Take delivery by December 31 to receive 3 months of FSD (Supervised)
+          and Supercharging.
           <a href="#" className="underline underline-offset-2 ml-1">
             Learn More
           </a>
@@ -62,16 +60,15 @@ const Configurator = () => {
       </TopBar>
       <Navbar mode="simple" />
       <main className="w-full flex flex-col md:flex-row justify-between md:py-6 h-dvh md:h-[calc(100dvh-108px)]">
-        {/* Fixed Carousel Navigation */}
-        <div className="fixed inset-0 flex justify-between px-4 z-30 items-center md:w-[70%]">
+        <div className="flex gap-2 top-[25%] md:top-1/2 left-0 z-30 md:w-[70%] w-full justify-between px-4 fixed ">
           <button
-            className="bg-dimmed-3 size-7 rounded-[4px] flex items-center justify-center cursor-pointer"
+            className={`bg-dimmed-3 size-7 rounded-[4px] flex items-center justify-center cursor-pointer`}
             onClick={() => handleCarousel("left")}
           >
             <ChevronLeftIcon className="size-4 text-black" />
           </button>
           <button
-            className="bg-dimmed-3 size-7 rounded-[4px] flex items-center justify-center cursor-pointer"
+            className={`bg-dimmed-3 size-7 rounded-[4px] flex items-center justify-center cursor-pointer`}
             onClick={() => handleCarousel("right")}
           >
             <ChevronLeftIcon className="size-4 text-black transform rotate-180" />
@@ -79,17 +76,20 @@ const Configurator = () => {
         </div>
 
         {/* Carousel */}
-        <div className="carousel w-full md:w-[70%] flex justify-center h-[40vh] bg-white md:shadow-none">
+        <div className="carousel w-full md:w-[70%] flex justify-center fixed top-0 z-10 h-[40vh] bg-white md:fixed md:top-auto md:-z-10 shadow-[0_5px_10px_0px_#ffffff] md:shadow-none">
           <img
             src={imgUrl || img}
             alt={`Image of ${name}`}
-            className={`transition-all ease-linear duration-300 bg-cover bg-center max-w-full scale-1 md:scale-[1.3] lg:scale-[1.9] md:h-96 ${imgUrl?.includes("interior") ? "!scale-x-[1.4] !scale-y-[1.7]" : ""
+            className={`bg-cover bg-center max-w-full scale-1 md:scale-[1.3] lg:scale-[1.9] md:h-96 ${imgUrl?.includes("interior") ? "md:!scale-x-[1.4] md:!scale-y-[1.7]" : ""
               }`}
           />
         </div>
 
+        {/* Space */}
+        <div className="space hidden md:w-[70%] h-full md:flex"></div>
+
         {/* Sidebar */}
-        <aside className="bg-white w-full md:w-[30%] flex flex-col items-center gap-5 relative mt-[30vh] md:mt-0">
+        <aside className="bg-white w-full md:w-[30%] flex flex-col items-center gap-5 relative mt-[35vh] md:mt-0">
           <h1 className="text-5xl font-medium font-display">{name}</h1>
           <div className="row text-gray-800 items-center justify-center gap-10">
             <div className="flex flex-col items-center justify-center">
